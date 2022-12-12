a1a85408b675bff64c71b0fd7e913c8ac9957289
rocketman15 <32797472+rocketman15@users.noreply.github.com>
2022-12-12 16:32:10 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# some of this code was taken from https://www.geeksforgeeks.org/caesar-cipher-in-cryptography/
+# this function will decrypt the message
+def decrypt(text,s):
+    result = ""
+    #calculate the shift used to decrypt the message by subtracting the origional shift from 26
+    decryptShift = 26 - s
+    # traverse text
+    for i in range(len(text)):
+        char = text[i]
+
+        # decrypt uppercase characters
+        if (char.isupper()):
+            result += chr((ord(char) + decryptShift-65) % 26 + 65)
+
+        # decrypt lowercase characters
+        else:
+            result += chr((ord(char) + decryptShift - 97) % 26 + 97)
+
+    return result
+#call the function for the different encrypted strings
+#simply coppy the following line of code once for every string to descrypt. then, replace the hashtags in the quotes with the string to decrypt, and replace
+#the lone hashtag with the shift used to encrypt the message. do not add any extra spaces or commas. 
+print(decrypt("#######", #))
